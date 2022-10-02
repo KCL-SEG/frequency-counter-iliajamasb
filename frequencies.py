2004187938186c46d10ded748b3f9dc317808280
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-02 11:55:08 +0100

    fully working

--- frequencies.py
+++ frequencies.py
@@ -5,6 +5,7 @@ def frequencies(items):
     frequencies = {}
 
     for item in items:
+        item = str(item)
         if item not in frequencies:
             frequencies[item] = 1
         else:
