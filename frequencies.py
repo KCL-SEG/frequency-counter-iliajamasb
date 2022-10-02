cdc77aa9cb459afe546813421e144bda92409b8b
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-02 11:47:24 +0100

    fixed bug

--- frequencies.py
+++ frequencies.py
@@ -5,10 +5,10 @@ def frequencies(items):
     frequencies = {}
 
     for item in items:
-        if item not in items:
-            items.item = 1
+        if item not in frequencies:
+            frequencies.item = 1
         else:
-            items.item += 1
+            frequencies.item += 1
     
     # Your code goes here
     print(items)
