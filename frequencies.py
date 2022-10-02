1015b138435d4dda95c00f98076b8288e09a1bc6
Ilia Jamasb <ilia@Ilias-Mac-mini.local>
2022-10-02 11:42:40 +0100

    Working function

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,15 @@
 
 def frequencies(items):
     frequencies = {}
+
+    for item in items:
+        if item not in items:
+            items.item = 1
+        else:
+            items.item += 1
+    
     # Your code goes here
+    print(items)
     return frequencies
+
+
